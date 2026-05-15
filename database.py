eb9b11b9b8423ba2f4aef42d46259c8ec2c523b9
unechueva <uliana.nechueva@gmail.com>
2026-05-15 18:57:21 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,17 +1,18 @@
 import sqlite3
+import pandas as pd
 
 conn = sqlite3.connect("recsys.db", check_same_thread=False)
 cursor = conn.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS interactions (
-    user_id INTEGER,
-    item_id INTEGER,
-    event TEXT
-)
-""")
-
-conn.commit()
+def init_db():
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS interactions (
+        user_id INTEGER,
+        item_id INTEGER,
+        event TEXT
+    )
+    """)
+    conn.commit()
 
 
 def add_interaction_db(user_id, item_id, event):
@@ -23,5 +24,4 @@ def add_interaction_db(user_id, item_id, event):
 
 
 def get_interactions_df():
-    import pandas as pd
     return pd.read_sql_query("SELECT * FROM interactions", conn)
