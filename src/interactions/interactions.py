bb8eea912130403252b2cdbdf6504c5e5921aefb
keaity <katuhahlamova1481@gmail.com>
2026-05-19 18:34:30 +0300

    Update interactions.py

--- src/interactions/interactions.py
+++ src/interactions/interactions.py
@@ -1,4 +1,2 @@
-from db_funcs import add_interaction_db
-
-def add_interaction(user_id, item_id, event):
-    add_interaction_db(user_id, item_id, event)
+"""Модуль взаимодействий"""
+# Основная логика перенесена в __init__.py
