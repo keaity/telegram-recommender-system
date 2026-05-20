1cf150a39e0a6b9f02f136e36ef11eb643296e0d
keaity <katuhahlamova1481@gmail.com>
2026-05-21 01:56:31 +0300

    Исправления в соответствии с PEP8

--- src/interactions/__init__.py
+++ src/interactions/__init__.py
@@ -6,4 +6,3 @@ from db_funcs import add_interaction_db
 def add_interaction(user_id: int, item_id: int, event: str) -> None:
     """Добавляет взаимодействие (view/like/fav)."""
     add_interaction_db(user_id, item_id, event)
-
